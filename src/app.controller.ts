d3ffa747cc25022fb910490fe3610ddaa8ac352c
gchaparroc <gchaparroc.certificaciones@gmail.com>
2023-03-01 03:57:13 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
+import { HasRoles } from './auth/has-roles.decorator';
+import { Role } from './model/role.enum';
+import { RolesGuard } from './auth/roles.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    //TODO: Consultar token de cuenta de servicio
+    //return req.user;
+  }
+
+  @HasRoles(Role.Admin)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('admin')
+  onlyAdmin(@Request() req) {
+    return req.user;
+  }
+
+  @HasRoles(Role.User)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('user')
+  onlyUser(@Request() req) {
+    return req.user;
+  }
+}
